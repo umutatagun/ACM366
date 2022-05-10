42ad00eeb574b580e2ba4c35038a39817f43ce3f
umutatagun <umutatgn@gmail.com>
2022-05-10 19:41:21 +0300

    düzenlemeler cart curt

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 var btc = document.getElementById("bitcoin");
 var eth = document.getElementById("ethereum");
-var doge = document.getElementById("dogecoin");
+var ada = document.getElementById("adacoin");
 
 
 var settings = {
     "async" :true,
     "scrossDomain":true,
-    "url":"https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=usd",
+    "url":"https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Ccardano&vs_currencies=usd",
     "method":"GET",
     "headers":{}
 }
 $.ajax(settings).done(function(res){
     btc.innerHTML = res.bitcoin.usd;
     eth.innerHTML = res.ethereum.usd;
-    doge.innerHTML = res.dogecoin.usd;
+    ada.innerHTML = res.cardano.usd;
 });
